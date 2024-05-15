1ebce7cd4eb4e78464df7697cbab161e5c5026bc
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-05-15 15:00:08 +0200

    Automatically open debug dialog if a warning/error occurs

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -9,6 +9,20 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    qInstallMessageHandler(&MainWindow::messageHandler);
+}
+
+void MainWindow::messageHandler(QtMsgType type, const QMessageLogContext& context, const QString& msg)
+{
+    if (type == QtWarningMsg || type == QtCriticalMsg || type == QtFatalMsg)
+	{
+        auto mainwindow = qobject_cast<MainWindow*>(QApplication::activeWindow());
+        mainwindow->onShowDebugOutput();
+
+        // Forward the message to the DebugOutputDialog
+        DebugOutputDialog::messageHandler(type, context, msg);
+	}
 }
 
 void MainWindow::onMapVisualization()
